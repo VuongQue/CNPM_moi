d34b5a70928ba9d2b3f601d6ce8dd3299b37c251
VuongQue <lapque.hcmute@gmail.com>
2025-08-29 15:33:13 +0700

    bài tập 4

---
+++ FullStackNodeJS01/ReactJS01/reactjs01/src/pages/home.jsx
@@ -0,0 +1,11 @@
+import { Result } from 'antd';
+import { CrownOutlined } from '@ant-design/icons';
+const HomePage = () => {
+    return (
+        <div style={{ padding: '20px' }}>
+            <Result icon={<CrownOutlined />} title="JSON Web Token (React/Node.js) - iotstar.vn" />
+        </div>
+    );
+}
+
+export default HomePage;
