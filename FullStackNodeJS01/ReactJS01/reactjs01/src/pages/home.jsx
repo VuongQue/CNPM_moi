b41945779e157b03044e9c33a1543ad76a51e6dd
VuongQue <lapque.hcmute@gmail.com>
2025-09-05 14:51:57 +0700

    Bài tập tuần 5

--- FullStackNodeJS01/ReactJS01/reactjs01/src/pages/home.jsx
+++ FullStackNodeJS01/ReactJS01/reactjs01/src/pages/home.jsx
@@ -1,11 +1,18 @@
-import { Result } from 'antd';
-import { CrownOutlined } from '@ant-design/icons';
+import { Result } from "antd";
+import { CrownOutlined } from "@ant-design/icons";
+import ProductList from "../components/layout/ProductList";
+
 const HomePage = () => {
-    return (
-        <div style={{ padding: '20px' }}>
-            <Result icon={<CrownOutlined />} title="JSON Web Token (React/Node.js) - iotstar.vn" />
-        </div>
-    );
-}
+  return (
+    <div style={{ padding: "20px" }}>
+      <Result
+        icon={<CrownOutlined />}
+        title="JSON Web Token (React/Node.js) - iotstar.vn"
+      />
+      {/* Thêm danh sách sản phẩm */}
+      <ProductList />
+    </div>
+  );
+};
 
 export default HomePage;
